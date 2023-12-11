a7b4a32b3f5d54b2b3d6187c033d45c36e0409f1
yonainthefish <yonaisgood@gmail.com>
2023-12-11 12:26:03 +0900

    :art: Design: module, 조건자를 사용한 스타일링

--- nomadcoders/nextjs_intro/components/NavBar.js
+++ nomadcoders/nextjs_intro/components/NavBar.js
@@ -1,15 +1,16 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import styles from './NavBar.module.css'
 
 export default function NavBar() {
   const router = useRouter()
   return (
     <nav>
       <Link href="/" passHref={true} legacyBehavior>
-        <a style={{ color: router.pathname === '/' ? 'red' : 'blue' }}>Home</a>
+        <a className={router.pathname === '/' ? styles.active : ''}>Home</a>
       </Link>
       <Link href="/about" passHref={true} legacyBehavior>
-        <a style={{ color: router.pathname === '/about' ? 'red' : 'blue' }}>
+        <a className={router.pathname === '/about' ? styles.active : ''}>
           About
         </a>
       </Link>
