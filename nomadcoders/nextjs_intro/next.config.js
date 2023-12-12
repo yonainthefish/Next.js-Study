74edcf2801b58fc1a58a0dbfece117f04f0bdfd5
yonainthefish <yonaisgood@gmail.com>
2023-12-13 02:27:33 +0900

    :recycle: Refactor: Redirect 와 Rewrite 사용법

--- nomadcoders/nextjs_intro/next.config.js
+++ nomadcoders/nextjs_intro/next.config.js
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const API_KEY = '3f1bf5f2d4fb226a512a677793bd3f14'
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/contact',
+        destination: '/form',
+        permanent: false,
+      },
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/movies',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ]
+  },
+}
